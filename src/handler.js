01c5b9efca0e5b74dfa1333fce7ff09e9e99a2f1
farz_fahd <harfanasamsung@gmail.com>
2024-04-25 08:39:29 +0700

    fix bug invalid date when add note

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,7 @@ const addNoteHandler = (request, h) => {
     const { title, tags, body } = request.payload;
     const id = nanoid(16)
 
-    const createdAt = new Date().toISOString;
+    const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
     const newNote = {
